8305ae6f1c609d25d6057255e6381dd140da8cb2
Tatiana1812 <79156300+Tatiana1812@users.noreply.github.com>
2023-02-28 17:03:39 +0300

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld firstBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(firstBean==secondBean);
+
+       Cat firstCat =
+                (Cat) applicationContext.getBean("cat");
+       Cat secondCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(firstCat==secondCat);
+    }
+}
